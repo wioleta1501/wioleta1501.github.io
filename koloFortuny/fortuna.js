4dd66f14950869466bf7a02881f5efc860af8662
wioleta1501 <wioleta1501@gmail.com>
2021-11-08 21:12:58 +0100

    added working game

--- koloFortuny/fortuna.js
+++ koloFortuny/fortuna.js
@@ -1,56 +1,136 @@
 let game = {
   zdobyte: 0,
-  zycia: 1,
+  zycia: 3,
 };
-let chooseCountry = getRandomInt(0, data.length)
-//alert(data[0]['country']);
-console.log(data[chooseCountry]['country']);
-let elem = document.getElementById("panstwa");
-elem.innerHTML = data[chooseCountry]['country'];
-
-//alert(data.length);
-//console.log(data.length);
-//alert(data[0]['country'][2]);
-console.log(data[chooseCountry]['country'][2]);
-
- for (let i = 0; i < data[chooseCountry]['country'].length; i += 1) {
-   //alert(data[0]['country'][i]);
-   console.log(data[chooseCountry]['country'][i]);
-  }
 
 
-addElement("wrap");
+
 //LISTENERS
 
-document.getElementById("graj").addEventListener("click", Sprawdz_Litery); 
+document.getElementById("graj").addEventListener("click", Sprawdz_Litery);
+document.getElementById("right").addEventListener("click", toggleAutor);
 //alert(game.zycia);
-console.log(game.zycia);
 
 
 //FUNKCJE
-function Sprawdz_Litery(){
+function startGame() {
+
+  let newDiv = document.createElement("button");
+  newDiv.classList.add('button');
+  newDiv.setAttribute("id", "start");
+  newDiv.innerHTML = "START";
+  newDiv.addEventListener("click", startBoard);
+  document.getElementById("panstwa").append(newDiv);
+
+  newDiv = document.createElement("button");
+  newDiv.classList.add('button');
+  newDiv.setAttribute("id", "autorB");
+  newDiv.innerHTML = "AUTOR";
+  newDiv.addEventListener("click", toggleAutor);
+  document.getElementById("panstwa").append(newDiv);
+
+}
+
+function toggleAutor() {
+  if (document.getElementById("autor").style.display === "none")
+    document.getElementById("autor").style.display = "";
+  else
+    document.getElementById("autor").style.display = "none";
+}
+
+function startBoard() {
+  document.getElementById("panstwa").innerHTML = "";
+  document.getElementById("podaj").style.display = "block";
+
+  let chooseCountry = getRandomInt(0, data.length)
+  console.log(data[chooseCountry]['country']);
+
+  let newDiv = document.createElement("div");
+  document.getElementById("panstwa").append(newDiv);
+  newDiv.classList.add("line");
+
+  for (let i = 0; i < 2; i += 1) {
+    addLetter(newDiv, "a", "unactive");
+  }
+  for (let i = 0; i < data[chooseCountry]['country'].length; i += 1) {
+    let style = "hidden"
+    if (data[chooseCountry]['country'][i].localeCompare(' ') === 0) {
+      for (let i = 0; i < 2; i += 1) {
+        addLetter(newDiv, "", "unactive");
+      }
+      newDiv = document.createElement("div");
+      document.getElementById("panstwa").append(newDiv);
+      newDiv.classList.add("line");
+      for (let i = 0; i < 2; i += 1) {
+        addLetter(newDiv, "", "unactive");
+      }
+    } else
+      addLetter(newDiv, data[chooseCountry]['country'][i], style);
+  }
+  for (let i = 0; i < 2; i += 1) {
+    addLetter(newDiv, "", "unactive");
+  }
+  changeLife(0);
+}
+
+function changeLife(change) {
+  game.zycia += change;
+  document.getElementById("zycie").innerHTML = ('Życia: ' + game.zycia);
+}
+
+function changePoints(change) {
+  game.zdobyte += change;
+  document.getElementById("punkty").innerHTML = ('Odgadnięte państwa: ' + game.zdobyte);
+}
+
+function Sprawdz_Litery() {
   let litera = document.getElementById("wpisz_litere").value;
-  //alert(litera);
-  console.log(litera);
-  //alert(getRandomInt(10,20));
-  console.log(getRandomInt(10,20));
+  let letters = document.getElementsByClassName("letter");
+  let guessed = false;
+  for (let i = 0; i < letters.length; i += 1) {
+    if (letters[i].innerHTML.toUpperCase().localeCompare(litera.toUpperCase()) === 0) {
+      letters[i].classList.remove("hidden");
+      guessed = true;
+    }
+  }
+
+  if (guessed) {
+    if (win()) {
+      alert("Wygrana!");
+      startBoard();
+      changeLife(5);
+      changePoints(1)
+    }
+  } else {
+    changeLife(-1);
+    if (game.zycia === 0) {
+      alert("Przegrana!");
+      startBoard()
+      changeLife(3);
+    }
+  }
+}
+
+function win() {
+  let letters = document.getElementsByClassName("letter");
+  for (let i = 0; i < letters.length; i += 1) {
+    if (letters[i].classList.contains("hidden")) {
+      return false;
+    }
+  }
+  return true;
 }
 
 
-function addElement(mydiv)
-{
+
+function addLetter(node, letter, style) {
 
   let newDiv = document.createElement("span");
-  newDiv.innerHTML = "jasiokotek";
+  newDiv.innerHTML = letter.toUpperCase();
+  node.append(newDiv);
 
-  let my_div = document.getElementById(mydiv);
-  document.body.insertBefore(newDiv, my_div);
-
-  let newDiv2 = document.createElement("span");
-  newDiv2.innerHTML = "jasiokotek2";
-  document.body.insertBefore(newDiv2, my_div.nextSibling);
-
-  newDiv.classList.add("mystyle");  
+  newDiv.classList.add("letter");
+  newDiv.classList.add(style);
 }
 
 function getRandomInt(min, max) {
@@ -58,3 +138,5 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+startGame();
